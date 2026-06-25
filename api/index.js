7799ff96adcfbbd111318cf6e9249e4a19e3cbcc
JCPrime01 <vanzokiller@gmail.com>
2026-06-25 09:56:10 -0300

    chore: shareCode BONEGNFX7CJ -> MTIWTE4TCAB (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=BONEGNFX7CJ&afp=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=MTIWTE4TCAB&afp=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
