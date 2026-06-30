c4c868564d991e6e3f1e1f27136e4d304c0a29ed
JCPrime01 <vanzokiller@gmail.com>
2026-06-30 12:38:26 -0300

    chore: shareCode NEW7XPX51US -> HPCZYU18LXC (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=NEW7XPX51US&afp=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=HPCZYU18LXC&afp=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
