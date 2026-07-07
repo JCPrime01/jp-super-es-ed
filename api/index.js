7d2352c6293dc95cdc2f37bcd5dd85656b033482
JCPrime01 <vanzokiller@gmail.com>
2026-07-07 09:07:08 -0300

    chore: shareCode G5M6JWQG41K -> CDIGO929784ZBLYF (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=G5M6JWQG41K&afp=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=CDIGO929784ZBLYF&afp=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
