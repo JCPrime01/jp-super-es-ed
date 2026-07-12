b123ce681c542e37acf771ac70051e2c815cfa2c
JCPrime01 <vanzokiller@gmail.com>
2026-07-12 10:26:40 -0300

    chore: shareCode D2X4Q502MGP -> 5G11APC7J93 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=D2X4Q502MGP&afp=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=5G11APC7J93&afp=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
