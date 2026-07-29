e7ae2bf1862525c0e59d7e661be30982471b081b
JCPrime01 <vanzokiller@gmail.com>
2026-07-29 10:45:43 -0300

    chore: shareCode EL5K4VG6YIJ -> R8FQNWTREUH (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=EL5K4VG6YIJ&afp=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=R8FQNWTREUH&afp=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
